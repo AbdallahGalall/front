be05deb662d75fc950afc5a7dc102f57712ff4a1
abdallah galal <abdallahgalal2003@gmail.com>
2024-07-19 18:09:33 +0300

    final

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let output = document.getElementById("out")
 let button= document.getElementById("click")
 //function converts tempreature degree from celsius to fahrenheit
 function s () {
-    output.value = input.value*(9/5)+32
+   output.value= input.value*(9/5)+32
 }
 
 //on clicking the function will be called
